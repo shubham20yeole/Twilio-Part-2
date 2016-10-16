dd2df53503dc5e4ab2da87ee374f4aa182ba62f6
Shubham Yeole <shubham20.yeole@gmail.com>
2016-10-15 23:13:50 -0400

    update message

--- src/main/java/com/shubham/controller/TwilioServlet.java
+++ src/main/java/com/shubham/controller/TwilioServlet.java
@@ -44,7 +44,7 @@ public class TwilioServlet extends HttpServlet {
     @SuppressWarnings({ "unchecked", "rawtypes" })
 	public String hashMapClass(String code){
     	String result = "I apologise, please try again with correct option.\n";
-    	String list ="\nList of subject is \n1 Biology \n2 Geometry \n3 Chemistry \n4 Algebra. \nReply with subject id.";
+    	String list ="\n\nList of subject is \n1 Biology \n2 Geometry \n3 Chemistry \n4 Algebra. \nReply with subject id.";
 
     	HashMap data = new HashMap();
     	data.put("start" ,"This is the SMS service designed by Shubham Yeole from Pace University Computer Science major. \n\n This SMS service assists guardians of high school students to learn about their children progress reports at school. \n\nIt also inform parents about the new facilities, events and future goals of school. \n\nPlease select the option given below. \n\nA Progress Report.\nB New facilities in school.\nC School future goals.\nSchool events. \n" );
@@ -56,19 +56,20 @@ public class TwilioServlet extends HttpServlet {
     	data.put("a","Thank you for your response. Please answer couple of questions and we will let you know the progress report of your student. \n\nPlease select the grade of the student.\n(Reply with any one digit of 5, 6, and 7)");
     	data.put("b","You have selected New Facility option.\n\nOur school is providing 3 new facilities to our students.\n1- Portable laptop to each student.\n2- Library with updated books\n3- School bus\n4- Entertainment hall.\n\nReply restart to start service again or reply with A or B or C charaters to learn about progress report, school new facilities, school future goals and school events respectively.");
     	data.put("B","You have selected New Facility option.\n\nOur school is providing 3 new facilities to our students.\n1- Portable laptop to each student.\n2- Library with updated books\n3- School bus\n4- Entertainment hall.\n\nReply restart to start service again or reply with A or B or C charaters to learn about progress report, school new facilities, school future goals and school events respectively.");
-    	data.put("c","You have selected school future goal option.\n\nGOAL 1: 100% of students participate in internships two days a week");
-    	data.put("5","Following are the names of students enrolled in 5th grade.\n 501 John Cena \n502 Abraham Bond \n503 Noa Capsi. \nPlease reply with the student id." );
-    	data.put("6","Following are the names of students enrolled in 6th grade.\n 601 Mich Leriche \n602 David P Perlmutter \n603 Tobias Edstrom. \nPlease reply with the student id." );
-    	data.put("7","Following are the names of students enrolled in 7th grade.\n 701 Dave Bush \n702 Daniel Bostos \n703 Rahul utada. \nPlease reply with the student id.");
-    	data.put("501","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 5011 for testing)");
-    	data.put("502","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 5022 for testing)");
-    	data.put("503","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 5033 for testing)");
-    	data.put("601","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 6011 for testing)");
-    	data.put("602","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 6022 for testing)");
-    	data.put("603","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 6033 for testing)");
-    	data.put("701","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 7011 for testing)");
-    	data.put("702","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 7022 for testing)");
-    	data.put("703","Please reply us with 4-digit pin. This pin code is required to keep students record confidential. (USE PIN 7033 for testing)");
+    	data.put("c","You have selected school future goal option.\n\nGOAL 1: 100% of students participate in internships two days a week.\nGOAL 2: Students increase reading level by at least one and a half grade levels.\nGOAL 3: 100% of students present oral exhibitions.\nGOAL 4: 90% of parents attend student exhibitions.\nGOAL 5: By 2017, expand the current XYZ school to serve 200 students.");
+    	data.put("C","You have selected school future goal option.\n\nGOAL 1: 100% of students participate in internships two days a week.\nGOAL 2: Students increase reading level by at least one and a half grade levels.\nGOAL 3: 100% of students present oral exhibitions.\nGOAL 4: 90% of parents attend student exhibitions.\nGOAL 5: By 2017, expand the current XYZ school to serve 200 students.");
+    	data.put("5","Thank you for selecting the grade of student. We would first need to know the name of your student before we present you with his/her reports.\n\n501 John Cena \n502 Abraham Bond \n503 Noa Capsi. \n\nPlease reply with the student id and we will proceed this to an authentication process...." );
+    	data.put("6","Thank you for selecting the grade of student. We would first need to know the name of your student before we present you with his/her reports.\n\n601 Mich Leriche \n602 David P Perlmutter \n603 Tobias Edstrom. \n\nPlease reply with the student id and we will proceed this to an authentication process...." );
+    	data.put("7","Thank you for selecting the grade of student. We would first need to know the name of your student before we present you with his/her reports.\n\n701 Dave Bush \n702 Daniel Bostos \n703 Rahul utada. \n\nPlease reply with the student id and we will proceed this to an authentication process...." );
+    	data.put("501","Please reply us with 4-digit pin for Mr. John Cena. This pin code is required to keep students record confidential. (USE PIN 5011 for testing)");
+    	data.put("502","Please reply us with 4-digit pin for Mr Abraham Bond . This pin code is required to keep students record confidential. (USE PIN 5022 for testing)");
+    	data.put("503","Please reply us with 4-digit pin for Mr Noa Capsi. This pin code is required to keep students record confidential. (USE PIN 5033 for testing)");
+    	data.put("601","Please reply us with 4-digit pin for Mr Mich Leriche. This pin code is required to keep students record confidential. (USE PIN 6011 for testing)");
+    	data.put("602","Please reply us with 4-digit pin for Mr David P Perlmutter. This pin code is required to keep students record confidential. (USE PIN 6022 for testing)");
+    	data.put("603","Please reply us with 4-digit pin for Mr Tobias Edstrom. This pin code is required to keep students record confidential. (USE PIN 6033 for testing)");
+    	data.put("701","Please reply us with 4-digit pin for Mr Dave Bush. This pin code is required to keep students record confidential. (USE PIN 7011 for testing)");
+    	data.put("702","Please reply us with 4-digit pin for Mr Daniel Bostos. This pin code is required to keep students record confidential. (USE PIN 7022 for testing)");
+    	data.put("703","Please reply us with 4-digit pin for Mr Ludmila Lupan. This pin code is required to keep students record confidential. (USE PIN 7033 for testing)");
     	data.put("5011","Mr John Cena is taking 4 different classes in this year. "+list);
     	data.put("5022","Mr Abraham Bond is taking 4 different classes in this year. "+list);
     	data.put("5033","Mr Noa Capsi is taking 4 different classes in this year. "+list);
